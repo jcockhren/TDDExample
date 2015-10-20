0c827253aa8d4024202d77aeeb1ead921d3aa992
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-19 17:46:53 -0700

    In class demonstration of Test Driven Development

---
+++ TDDExample/Addition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExample
+{
+    public class Addition
+    {
+        public int Add(int x,int y) // Argument names are placeholders
+        {
+            return x + y;
+        }
+
+        public int AddStrings(string a,string b)
+        {
+            int output;
+            int j;
+            int k;
+            if (!Int32.TryParse(a, out j)) {
+                ArgumentException argEx = new ArgumentException();
+                throw argEx;
+            }
+
+            if (!Int32.TryParse(b, out k)) // Throw Exception when TryParse is NOT true.
+            {
+                ArgumentException argEx = new ArgumentException();
+                throw argEx;
+            }
+            /*output = j + k;
+            return output;*/
+            
+            return Add(j, k); // This is the method from Above.
+        }
+    }
+}
---
+++ TTDExampleTests/AdditionTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDExample; // This is so we can see the 'Addition' class
+
+namespace TTDExampleTests
+{
+    [TestClass]
+    public class AdditionTest
+    { 
+        // Bootstrapping test
+        [TestMethod]
+        public void TestCanCreateAdditionInstance()
+        {
+            Addition operation_class = new Addition();
+        }
+
+        [TestMethod]
+        public void TestCanAddTwoIntegers()
+        {
+            int input1 = 1;
+            int input2 = 2;
+            int expected = 3;
+            Addition operation_class = new Addition();
+            Assert.AreEqual(expected, operation_class.Add(input1, input2)); // "Add" must be defined.
+        }
+
+        [TestMethod]
+        public void CanAddTwoNegativeNumbers()
+        {
+            int input1 = -1;
+            int input2 = -4;
+            int expected = -5;
+            Addition operation_class = new Addition();
+            Assert.AreEqual(expected, operation_class.Add(input1, input2));
+        }
+
+        [TestMethod] // Notes on how the Add method works.
+        public void CanNotAddAnIntegerToAString()
+        {
+            // Ahh. The Add method can't accept string as input.
+            int input1 = -1;
+            string input2 = "0"; 
+            int expected = -5;
+            Addition operation_class = new Addition();
+            //Assert.AreEqual(expected, operation_class.Add(input1, input2));
+        }
+
+        [TestMethod]
+        public void CanAddTwoStringsGoodInput()
+        {
+            string input1 = "2";
+            string input2 = "1";
+            int expected = 3;
+            Addition operation_class = new Addition();
+            Assert.AreEqual(expected, operation_class.AddStrings(input1, input2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddTwoStringsBadInput()
+        {
+            string input1 = "1";
+            string input2 = "kjwjrjkrjkerjk";
+            Addition operation_class = new Addition();
+            operation_class.AddStrings(input1,input2); // This is not an Assert
+        }
+    }
+}
